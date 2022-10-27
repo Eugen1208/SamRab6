b48930b81fb5f18bc805b6de7f34722514654ee8
User <User@E-NOTE>
2022-10-27 21:31:45 +0500

    Добавьте файлы проекта.

---
+++ Task2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            string str2 = "";
+            str = str.Replace(" ", "");
+            foreach (char c in str)
+            {
+                str2 = c + str2;
+            }
+            Console.WriteLine(str2);
+            if (str == str2)
+            {
+                Console.WriteLine("Данная строка является палиндромом");
+            }
+            if (str != str2)
+            {
+                Console.WriteLine("Данная строка не является палиндромом");
+            }
+            Console.ReadKey();
+        }
+    }
+}
